7657d035a23740fe9ab76ceaa2f638ccfa123981
jvidal <javier.vidal@davinci.edu.ar>
2024-04-27 02:28:45 -0300

    feat: getPokemons(url) added

---
+++ src/services/pokemon.js
@@ -0,0 +1,15 @@
+/**
+ * This function looks for pokemons data within the endpoint param
+ * @param {} url  url for a certain amount of pokemons
+ */
+export const getPokemons = (url) => {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Pokemons no encontrados");
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch(console.log(Error));
+};

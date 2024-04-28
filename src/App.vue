e24f985b1f7ddda041785aca6c734cd54eeefe3b
jvidal <javier.vidal@davinci.edu.ar>
2024-04-28 05:41:09 -0300

    feat: new font added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
+import RangeSearch from "./components/RangeSearch.vue";
 </script>
 
 <!-- Template -->
 <template>
   <header>
     <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
+      <nav class="flex gap-2 justify-center p-4 border-b-2 border-b-red-600">
+        <RouterLink class="bg-gray-950 text-gray-50 p-1 border-red-500 border-solid border-2" to="/">Home</RouterLink>
+        <RouterLink class="bg-gray-950 text-gray-50 p-1 border-red-500 border-solid border-2" to="/history">History</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <main>
+    <h1 class="text-4xl text-center font-bold p-4">Pokemon Viewer</h1>
+    <div>
+      <RangeSearch />
+    </div>
+    <RouterView />
+  </main>
 </template>
 
 <!-- Styles -->
 <style scoped>
-*{
-  border: #232323 0.5px dashed;
+h1{
+  font-family: 'Pokemon GB',sans-serif;
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html",
-  "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  "./src/**/*.{vue,js,ts,jsx,tsx,ttf,woff,eot,svg}"],
   theme: {
     extend: {},
   },

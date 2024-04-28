a56da48eee62816ddbebb9c028cb875c7b88f430
jvidal <javier.vidal@davinci.edu.ar>
2024-04-28 05:34:46 -0300

    feat: PokeHistory.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import PokeView from "../views/PokeView.vue";
+import PokeHistory from '../views/PokeHistory.vue';
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: PokeView,
+  },
+  {
+    path: "/history",
+    name: "history",
+    component: PokeHistory,
   },
 ];
 const router = createRouter({
---
+++ src/views/PokeHistory.vue
@@ -0,0 +1,13 @@
+<script>
+import HeaderTwo from '../components/headers/HeaderTwo.vue';
+
+export default {
+    name: 'PokeHistory',
+    components: {HeaderTwo}
+}
+</script>
+<template>
+    <div>
+        <HeaderTwo>Pokemon History Viewer</HeaderTwo>
+    </div>
+</template>

0ee339e4ba54a376d6ba79603f21313a1d379e73
jvidal <javier.vidal@davinci.edu.ar>
2024-04-28 05:38:04 -0300

    change: RangeSearch template added

--- src/components/RangeSearch.vue
+++ src/components/RangeSearch.vue
@@ -1,13 +1,21 @@
 <script>
 export default {
-    name:'RangeSearch',
-    data(){
-        return{
-            
-        }
-    }
+  name: "RangeSearch",
+  data() {
+    return {};
+  },
 };
 </script>
 <template>
-  <div></div>
+  <div>
+    <form action="#" method="get" enctype="multipart/form-data"
+    class="bg-slate-100 p-2" 
+    >
+      <label class="flex gap-2 justify-center" for="poke_amount"
+        ><p>Cantidad a buscar:</p>
+        <input type="number" id="poke_amount" min="1" max="100" class="border-green-200" />
+        <span>(máximo 100)</span>
+      </label>
+    </form>
+  </div>
 </template>

51af9f0f6e3d6a6e3b57196e6d899026a001512e
jvidal <javier.vidal@davinci.edu.ar>
2024-05-01 19:32:49 -0300

    change: Pokemon.vue to PokemonCard.vue
    So it can be used in templates as a <kebab-case> tag (<pokemon-card></pokemon-card>

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "PokemonCard",
+  props: { objectPokemon: Object },
+  data() {
+    return {
+      // data: "",
+      url: this.objectPokemon.url,
+      name: this.objectPokemon.name,
+      // avatar: "",
+      // types: [],
+      // icon: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    /**
+     *
+     */
+    // getAvatar() {
+    //   this.avatar = this.data.sprites.front_default;
+    // },
+    // /**
+    //  *
+    //  */
+    // getType() {
+    //   this.types = this.data.types;
+    // },
+  },
+};
+</script>
+<template>
+  <div>
+    <h3>Pokemon: {{ name }}</h3>
+    <p>Url: {{ url }}</p>
+    <!-- <div class="image">
+      <img :src="avatar" alt="Pokemon Image" />
+    </div> -->
+  </div>
+</template>

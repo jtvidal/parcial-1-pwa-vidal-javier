34459cbd15572306ed4a3ebffcb44f3421d8679e
jvidal <javier.vidal@davinci.edu.ar>
2024-05-06 20:48:34 -0300

    feat: PokeHistory clear

--- src/components/NameSearch.vue
+++ src/components/NameSearch.vue
@@ -9,7 +9,7 @@ export default {
     class="border-double border-slate-950 border-[1rem] rounded-lg w-full xsm:w-2/4"
   >
     <form
-      class="flex flex-col gap-4 justify-between border-solid border-4 border-slate-950 h-full p-2"
+      class=" flex flex-col gap-4 justify-between border-solid border-4 border-slate-950 h-full p-2"
       action="#"
       method="get"
       enctype="multipart/form-data"
@@ -17,7 +17,7 @@ export default {
       <label class="flex flex-col gap-2 items-center" for="poke_name"
         ><p>Search by name</p>
         <input
-          class="w-11/12 border-2 border-gray-300 bg-gray-300 text-gray-100 ps-2"
+          class="text-zinc-800 focus:border-rose-600 focus:outline-none w-11/12 border-2 border-gray-300 bg-gray-300 ps-2"
           type="search"
           id="poke_name"
           name="poke_name"
--- src/views/PokeHistory.vue
+++ src/views/PokeHistory.vue
@@ -4,7 +4,7 @@ import HeaderTwo from "../components/headers/HeaderTwo.vue";
 
 export default {
   name: "PokeHistory",
-  components: { HeaderTwo, PokemonCards},
+  components: { HeaderTwo, PokemonCards },
   props: { arrayPokemon: Array },
   data() {
     return {
@@ -17,12 +17,21 @@ export default {
       console.log(this.billPc);
     }
   },
+  methods: {
+    clearStorage() {
+      localStorage.clear();
+      window.location.reload();
+    },
+  },
 };
 </script>
 <template>
   <div>
     <div>
       <HeaderTwo>POKéMON HISTORY VIEW</HeaderTwo>
+    </div>
+    <div class="w-8/12 sm:w-2/6 flex justify-center mx-auto">
+      <button @click="clearStorage" class="bg-rose-700 font-pokemon w-full p-2 text-zinc-100 cursor-pointer rounded-lg hover:bg-rose-500">Clear</button>
     </div>
     <div>
       <pokemon-cards :array-pokemon="billPc"></pokemon-cards>

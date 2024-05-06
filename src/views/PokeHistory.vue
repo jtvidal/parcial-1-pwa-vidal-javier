ed2a477593378615e09cf17485d2fff84a7c176d
jvidal <javier.vidal@davinci.edu.ar>
2024-05-06 20:32:46 -0300

    feat: PokeHistory shows pokemon

--- src/views/PokeHistory.vue
+++ src/views/PokeHistory.vue
@@ -1,18 +1,31 @@
 <script>
-import HeaderTwo from '../components/headers/HeaderTwo.vue';
+import PokemonCards from "@/components/PokemonCards.vue";
+import HeaderTwo from "../components/headers/HeaderTwo.vue";
 
 export default {
-    name: 'PokeHistory',
-    components: {HeaderTwo},
-    data(){
-        return{
-            billPc:[],
-        }
+  name: "PokeHistory",
+  components: { HeaderTwo, PokemonCards},
+  props: { arrayPokemon: Array },
+  data() {
+    return {
+      billPc: [],
+    };
+  },
+  mounted() {
+    if (localStorage.data) {
+      this.billPc = JSON.parse(localStorage.getItem("data"));
+      console.log(this.billPc);
     }
-}
+  },
+};
 </script>
 <template>
+  <div>
     <div>
-        <HeaderTwo>POKéMON HISTORY VIEW</HeaderTwo>
+      <HeaderTwo>POKéMON HISTORY VIEW</HeaderTwo>
     </div>
+    <div>
+      <pokemon-cards :array-pokemon="billPc"></pokemon-cards>
+    </div>
+  </div>
 </template>

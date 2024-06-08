90c3e838f06bad2d842ca7e56e7f54c9234d71e9
jvidal <javier.vidal@davinci.edu.ar>
2024-06-08 12:47:59 -0300

    feat: service worker registered.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import './main.css'
+import "./main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("poke-worker.js").then((registration) => {
+    console.log("poke service worker registered", registration);
+  });
+}
